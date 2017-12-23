906f9fe6baa737b2ed8349828b6079fdab36da28
VsUcode <vsu-email@foxmail.com>
2017-12-23 09:07:20 +0800

    behavior

---
+++ behavior/memorandum/Test.java
@@ -0,0 +1,103 @@
+package com.vsu.behavior.memorandum;
+
+/**
+ * Created by vsu on 2017/12/23.
+ */
+
+/**
+ * 备忘录模式
+ */
+
+
+
+class Original {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Original(String value) {
+        this.value = value;
+    }
+
+    public Memento createMemento(){
+        return new Memento(value);
+    }
+
+    public void restoreMemento(Memento memento){
+        this.value = memento.getValue();
+    }
+}
+
+
+class Memento {
+
+    private String value;
+
+    public Memento(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
+
+
+
+class Storage {
+
+    private Memento memento;
+
+    public Storage(Memento memento) {
+        this.memento = memento;
+    }
+
+    public Memento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
+
+
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        // 创建原始类
+        Original origi = new Original("egg");
+
+        // 创建备忘录
+        Storage storage = new Storage(origi.createMemento());
+
+        // 修改原始类的状态
+        System.out.println("初始化状态为：" + origi.getValue());
+        origi.setValue("niu");
+        System.out.println("修改后的状态为：" + origi.getValue());
+        /*
+        初始化状态为：egg
+        修改后的状态为：niu
+         */
+
+        // 回复原始类的状态
+        origi.restoreMemento(storage.getMemento());
+        System.out.println("恢复后的状态为：" + origi.getValue());
+        /**
+         * 恢复后的状态为：egg
+         */
+    }
+}
